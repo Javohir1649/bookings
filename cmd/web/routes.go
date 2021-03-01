8d7865171cb246b31c223bf962129bb290fbf9b2
Javokhir <66038187+Javohir1649@users.noreply.github.com>
2021-03-01 15:46:41 -0500

    saving

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -1,10 +1,10 @@
 package main
 
 import (
-	"github.com/Javohir1649/bookings/pkg/config"
-	"github.com/Javohir1649/bookings/pkg/handlers"
 	"net/http"
 
+	"github.com/Javohir1649/bookings/pkg/config"
+	"github.com/Javohir1649/bookings/pkg/handlers"
 	"github.com/go-chi/chi"
 	"github.com/go-chi/chi/middleware"
 )
@@ -25,5 +25,7 @@ func routes(app *config.AppConfig) http.Handler {
 	mux.Get("/", handlers.Repo.Home)
 	mux.Get("/about", handlers.Repo.About)
 
+	fileServer := http.FileServer(http.Dir("../../static/"))
+	mux.Handle("/static/*", http.StripPrefix("/static", fileServer))
 	return mux
 }
